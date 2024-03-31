5bf9b99da8f1660dd348b6c96c42823d4c846395
Nainaadhingra <104780798+Nainaadhingra@users.noreply.github.com>
2024-03-31 12:48:14 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import MainApp from './Components/MainApp';
+import AddUser from './Components/AddUser';
+import Users from './Components/Users';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import ReactApp from './Components/ReactApp';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Welcome to React App</p>
+      
+      <ReactApp/>    
+    </div>
+
+  );
+}
+
+export default App;
